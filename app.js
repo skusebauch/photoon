114b84cb030ff6bcd265835b6ecde6a00fb5282b
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-21 17:47:59 +0200

    get rid of search clear and download function

--- app.js
+++ app.js
@@ -15,54 +15,54 @@ function updateInput(event) {
   searchValue = event.target.value;
 }
 
-// Getting Photos "curated photos from the doc"
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=15?page=1",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
-  );
+// refactor fetch header
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
   const data = await dataFetch.json();
+  return data;
+}
+// refactor HTML MARKUP of the pictures
+function generatePicsMarkup(data) {
   data.photos.forEach((photo) => {
-    console.log(photo);
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `
+      <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a href="${photo.src.original}">Download</a>
+      </div>
       <img src="${photo.src.large}"></img>
-      <p>${photo.photographer}</p>
     `;
     gallery.appendChild(galleryImg);
   });
 }
 
+// Getting Photos "curated photos from the doc"
+async function curatedPhotos() {
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15?page=1"
+  );
+  generatePicsMarkup(data);
+}
+
 // Getting phots from searchinput
 async function searchPhotos(search) {
-  const dataFetch = await fetch(
-    `https://api.pexels.com/v1/search?query=${search}&per_page=15?page=1`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
+  clearSearch();
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${search}&per_page=15?page=1`
   );
-  const data = await dataFetch.json();
-  data.photos.forEach((photo) => {
-    console.log(photo);
-    const galleryImg = document.createElement("div");
-    galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `
-      <img src="${photo.src.large}"></img>
-      <p>${photo.photographer}</p>
-    `;
-    gallery.appendChild(galleryImg);
-  });
+  generatePicsMarkup(data);
+}
+
+function clearSearch() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 
 curatedPhotos();
